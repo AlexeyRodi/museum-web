b92dc21ad760946e1bba895f7ea0e63c0b33a000
AlexeyRodi <kolohok@yandex.ru>
2024-11-18 22:53:30 +0300

    edit rooms_list.html

--- museum/main/urls.py
+++ museum/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index, name='index'),
+    path('комнаты/',views.rooms_list, name='rooms_list'),
 ]
--- museum/main/views.py
+++ museum/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'main/index.html')
+
+def rooms_list(request):
+    return render(request,'main/rooms-list.html')
