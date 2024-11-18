8a8aec1d0f985782f227e7c2963b3c82f361429a
AlexeyRodi <kolohok@yandex.ru>
2024-11-19 00:06:45 +0300

    connect database,generate models and make migration

--- museum/main/models.py
+++ museum/main/models.py
@@ -1,3 +1,102 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+
 from django.db import models
 
-# Create your models here.
+
+class Exhibit(models.Model):
+    exhibit_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    creation_year = models.IntegerField(blank=True, null=True)
+    creator = models.CharField(max_length=255, blank=True, null=True)
+    room = models.ForeignKey('MuseumRoom', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey('Users', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'exhibit'
+
+
+class ExhibitExhibition(models.Model):
+    exhibit = models.OneToOneField(Exhibit, models.DO_NOTHING, primary_key=True)  # The composite primary key (exhibit_id, exhibition_id) found, that is not supported. The first column is selected.
+    exhibition = models.ForeignKey('Exhibition', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'exhibit_exhibition'
+        unique_together = (('exhibit', 'exhibition'),)
+
+
+class Exhibition(models.Model):
+    exhibition_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    country = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    venue = models.CharField(max_length=255, blank=True, null=True)
+    responsible_person = models.CharField(max_length=255, blank=True, null=True)
+    museum = models.ForeignKey('Museum', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'exhibition'
+
+
+class ExhibitionCategory(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'exhibition_category'
+
+
+class ExhibitionExhibitionCategory(models.Model):
+    exhibition = models.OneToOneField(Exhibition, models.DO_NOTHING, primary_key=True)  # The composite primary key (exhibition_id, category_id) found, that is not supported. The first column is selected.
+    category = models.ForeignKey(ExhibitionCategory, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'exhibition_exhibition_category'
+        unique_together = (('exhibition', 'category'),)
+
+
+class Museum(models.Model):
+    museum_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    location = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'museum'
+
+
+class MuseumRoom(models.Model):
+    room_id = models.AutoField(primary_key=True)
+    room_number = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    museum = models.ForeignKey(Museum, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'museum_room'
+
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=255)
+    password = models.TextField()
+    role = models.TextField()  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'users'
+
--- museum/museum/settings.py
+++ museum/museum/settings.py
@@ -17,7 +17,6 @@ from django.conf.global_settings import STATICFILES_DIRS
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'django-insecure-bdcan(-@*xvzn6rim=lfn8zck1jqpquc1d_hlwp5rx3wevdbl*
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,17 +70,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'museum.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -102,11 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -114,14 +113,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "main/static",
 ]
 
 # Default primary key field type
